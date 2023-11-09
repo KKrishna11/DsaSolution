8964f41fd6a0da9081beccba8f7810b0c35c61aa
KKrishna11 <97391972+KKrishna11@users.noreply.github.com>
2023-11-09 19:15:56 +0530

    Time: 151 ms (30.36%) | Memory: 50.1 MB (93.43%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -2,8 +2,20 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
-    var s=x.toString()
-    var t=s.split("").reverse().join("")
-    return s===t
+var isPalindrome = function (x) {
+    // var s = x.toString()
+    // var t = s.split("").reverse().join("")
+    // return s === t
+
+    if(x<0)
+    {
+        return false
+    }
+    let reverse = 0;
+    for(let i=x;i>=1;i=Math.floor(i/10))
+    {
+        reverse=reverse*10+i%10
+    }
+    return reverse===x
+
 };
