8692c3617781d631d93fc4481a45d93f77123467
KKrishna11 <97391972+KKrishna11@users.noreply.github.com>
2023-12-04 17:49:04 +0530

    Time: 66 ms (41.32%) | Memory: 42.1 MB (21.41%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    if( n==1 || n==2){
+        return 1 
+    }
+    else if(n==0){
+        return 0;
+    }
+    else{
+
+    return fib(n-1) +fib(n-2);
+    }
+};
